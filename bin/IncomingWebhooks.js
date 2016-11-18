0316d4b7190c0306385f885f186859f9bb37db5f
KilllianB <killian.barreau@ynov.com>
2016-11-18 10:59:00 +0100

    Adding
    Adding new project

---
+++ bin/IncomingWebhooks.js
@@ -0,0 +1,17 @@
+var IncomingWebhooks = require('@slack/client').IncomingWebhook;
+// Anyone who has access to this url will be able to post your
+// slack org without authentication. So don't save this value in version control
+var wh = new IncomingWebhooks('https://hooks.slack.com/services/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', {
+    username: 'Custom Username',
+    channel: '#general',
+    iconEmoji: ':robot_face:',
+    text: 'Default Text'
+});
+// This will send a message "Some Text" using the configuration
+// chosen when creating the webhook
+wh.send('Some text');
+
+// You can pass an optional callback
+wh.send('More text', function () {
+    console.log('done sending');
+});
---
+++ bin/RTMClient.js
@@ -0,0 +1,42 @@
+var RtmClient = require('@slack/client').RtmClient;
+var token = 'xoxb-93294615989-UAgHilcMqsfzeRZZeaCOz423';
+var MemoryDataStore = require('@slack/client').MemoryDataStore;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+//var text = require('@slack/client').RTM_EVENTS.MESSAGE
+var rtm = new RtmClient(token, {
+    // Sets the level of logging we require
+    logLevel: "error",
+    // Initialise a data store for our client, this will load additional helper functions for the storing and retrieval of data
+    dataStore: new MemoryDataStore()
+});
+
+//Authentication - DO NOT DELETE
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+// INITIALIZATION - DO NOT DELETE
+rtm.start();
+
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+    // Listens to all `message` events from the team
+});
+
+// Wait for the client to connect - DO NOT DELETE
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+    // Get the user's name
+    var user = rtm.dataStore.getUserById(rtm.activeUserId);
+    // Get the team's name
+    var team = rtm.dataStore.getTeamById(rtm.activeTeamId);
+    // Log the slack team name and the bot's name
+    console.log('Connected to ' + team.name + ' as ' + user.name);
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+    //Message handler : Logs of all messages sent in channels
+    console.log('Message : ' + message.text + '\nFrom : ' + message.user + '\nIn : ' + message.channel);
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+    rtm.sendMessage("Hello <@" + message.user + ">!" + " By sending : " + message.text, message.channel);
+});
