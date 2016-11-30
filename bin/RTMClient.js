197300dbb13c6cb90fe91906fe12a5e40668dca6
Killian <killian.barreau@ynov.com>
2016-11-30 09:56:34 +0100

    Resolving a bug on rss feeds(step 2)

--- bin/RTMClient.js
+++ bin/RTMClient.js
@@ -1,5 +1,5 @@
 var RtmClient = require('@slack/client').RtmClient;
-var token = process.env.OPENSHIFT_ENV_VAR; // Can't integrate the token in repository -> Failed to auth
+var token = process.env.OPENSHIFT_ENV_VAR || 'xoxb-93294615989-pRyWzyswYeJeKW6vDfYxIZnr'; // Can't integrate the token in repository -> Failed to auth
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
@@ -22,32 +22,26 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
     // Listens to all `message` events from the team
 });
 
-rtm.on(rsi.start({
+rtm.on(rsi.start({ //To test, will probably result in an error -> Can't start simultaneous rss events
     //Ynov Nantes
     feed: "https://www.facebook.com/feeds/page.php?format=rss20&id=1583788541925285",
     interval: 3600,
     slackHook: "https://hooks.slack.com/services/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",
     slackIcon: "http://s1.narvii.com/image/tqhm2jdefu4cxmczyg4mknnjata2s4pk_128.jpg",
     slackBotUser: "Awesome Bot",
-}));
-
-/*rtm.on(rsi.start({
     //BDE Ynov
     feed: "https://www.facebook.com/feeds/page.php?format=rss20&id=1057210290979698",
     interval: 3600,
     slackHook: "https://hooks.slack.com/services/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",
     slackIcon: "http://s1.narvii.com/image/tqhm2jdefu4cxmczyg4mknnjata2s4pk_128.jpg",
-    slackBotUser: "Awesome Bot"
-}));
-
-rtm.on(rsi.start({
+    slackBotUser: "Awesome Bot",
     //BDS Ynov
     feed: "https://www.facebook.com/feeds/page.php?format=rss20&id=1218659048150191",
     interval: 3600,
     slackHook: "https://hooks.slack.com/services/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",
     slackIcon: "http://s1.narvii.com/image/tqhm2jdefu4cxmczyg4mknnjata2s4pk_128.jpg",
     slackBotUser: "Awesome Bot"
-}));*/
+}));
 
 
 // Wait for the client to connect - DO NOT DELETE
