f712da769c11ed4d0f93233e98fdb167f7741101
Killian <killian.barreau@ynov.com>
2016-12-01 15:52:49 +0100

    Applying changes about anwsering

--- bin/RTMClient.js
+++ bin/RTMClient.js
@@ -15,7 +15,7 @@ var rtm = new RtmClient(token, {
 
 //Authentication - DO NOT DELETE
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+    console.log("Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel");
 });
 // INITIALIZATION - DO NOT DELETE
 rtm.start();
